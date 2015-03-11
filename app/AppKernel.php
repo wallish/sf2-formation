6ce0c7afaeff19ebb36622b0f2b4a43b65166de9
fanorazafindramaro <fanorazafindramaro@10.32.1.50>
2015-03-11 16:24:58 +0100

    Installation Sonata admin bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,13 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new ESGI\BlogBundle\ESGIBlogBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
-            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle()
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/ESGI/BlogBundle/Admin/CategoryAdmin.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ESGI\BlogBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class CategoryAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name', 'text', array('label' => 'Category name'))
+//            ->add('author', 'entity', array('class' => 'Acme\DemoBundle\Entity\User'))
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name')
+//            ->add('author')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('name')
+//            ->add('author')
+        ;
+    }
+}
